2d3e39d88946b1d8f7a382b079739e401e59759d
Magno <olivmagno1@gmail.com>
2023-07-28 17:47:37 -0300

    removed redundant components

--- src/components/WhoWeAre/Mission.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <div>LOGO</div>
-  <h4>Missão</h4>
-  <p>
-    Oferecer aos nossos clientes um atendimento Humanizado
-    e de qualidade, e oferecer a solução completa em
-    movimentação de cargas, de forma eficiente e dinâmica.
-  </p>
-</template>
