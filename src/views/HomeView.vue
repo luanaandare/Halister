22bde967674e4c48f7f349321e6a0b6e51d34b87
Magno <olivmagno1@gmail.com>
2023-07-31 17:43:40 -0300

    added button hiding on top of the page

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -7,18 +7,24 @@
     <Parts />
     <Services />
     <Brands />
-    <a
-      href="#home"
-      class="hoveringButton alignLeft"
-    >Voltar</a>
+    <Transition name="fade">
+      <a
+        v-show="scrollpx > 700"
+        href="#home"
+        class="hoveringButton alignLeft"
+        title="Voltar para o topo da página"
+      > Voltar </a>
+    </Transition>
     <a
       href="https://wa.me/5511984574754?text=Olá%2C%20estive%20no%20seu%20site%20e%20tenho%20interesse%20em%20conhecer%20melhor%20vocês!"
       class="hoveringButton alignRight"
+      title="Contate-nos no whatsapp!"
     ><img src="../assets/images/icons/wpp.png"></a>
   </main>
 </template>
 
 <script>
+import { ref } from 'vue';
 import HeaderMenu from "../components/HeaderMenu.vue";
 import BackgroundIntro from "../components/websiteSections/BackgroundIntro.vue";
 import WhoWeAre from "../components/websiteSections/WhoWeAre.vue";
@@ -36,6 +42,22 @@ export default {
     Parts,
     Services,
     Brands,
+  },
+  data() {
+    const scrollpx = ref(0);
+
+    return { scrollpx }
+  },
+  destroyed() {
+    window.removeEventListener('scroll', this.actionScroll);
+  },
+  created() {
+    window.addEventListener('scroll', this.actionScroll);
+  },
+  methods:{
+    actionScroll (event) {
+      this.scrollpx = window.scrollY;
+    },
   },
 };
 </script>
@@ -55,11 +77,8 @@ export default {
   position: fixed;
   bottom: 3rem;
   transition: 0.2s;
-}
-
-a:hover {
-  background-color: rgba(0, 0, 0, 0);
-  transform: scale(1.05);
+  width: 4rem;
+  height: 4rem
 }
 
 .hoveringButton img {
@@ -68,10 +87,36 @@ a:hover {
 }
 
 .alignLeft {
+  border-radius: 0.8rem;
   left: 3rem;
+  background-color: #ddd;
+  text-decoration: none;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.alignLeft:hover {
+  transform: scale(1.05);
 }
 
 .alignRight {
   right: 3rem;
+}
+
+.alignRight:hover {
+  background-color: rgba(0, 0, 0, 0);
+  transform: scale(1.05);
+}
+
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.2s ease;
+}
+
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
 }
 </style>
