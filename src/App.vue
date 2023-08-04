e4dda36d98a836bcb775645186768e9dc3dadf0d
Magno <olivmagno1@gmail.com>
2023-08-04 13:52:52 -0300

    removed external css

--- src/App.vue
+++ src/App.vue
@@ -6,3 +6,50 @@ import { RouterView } from 'vue-router'
 <template>
   <RouterView />
 </template>
+
+<style>
+/* color palette from <https://github.com/vuejs/theme> */
+:root {
+  --halister-theme-white: #ffffff;
+  --halister-theme-lightgray: #626262;
+  --halister-theme-darkgray: #2d2d2d;
+  --halister-theme-yellow: #f2c60f;
+  --halister-theme-darkyellow: #d49a0e;
+
+  --color-text-dark: var(--halister-theme-darkgray);
+  --color-text-light: var(--halister-theme-white);
+  --color-highlight: var(--halister-theme-yellow);
+  --color-background: var(--halister-theme-darkgray);
+}
+
+* {
+  margin: 0;
+  padding: 0;
+  scroll-behavior: smooth;
+  max-width: 100vw;
+}
+
+@font-face {
+  font-family: "BebasNeue";
+  src: url("./assets/fonts/BebasNeue.ttf");
+}
+
+@font-face {
+  font-family: "SourceSans";
+  src: url("./assets/fonts/SourceSans.ttf");
+}
+
+body {
+  min-height: 100vh;
+  overflow-x: hidden;
+  position: relative;
+  background: var(--halister-theme-darkgray);
+  transition: color 0.5s, background-color 0.5s;
+  line-height: 1.6;
+  font-family: "SourceSans", sans-serif;
+  font-size: 1rem;
+  text-rendering: optimizeLegibility;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+</style>
