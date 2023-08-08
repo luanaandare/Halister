5c79214a5af6400b6ae54853f9a968a35a767534
Magno <olivmagno1@gmail.com>
2023-08-08 16:21:41 -0300

    fixed floating buttons range to show/hide

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -13,7 +13,7 @@
     <FooterMenu />
     <Transition name="fade">
       <a
-        v-if="scrollpx > 0.29 * iw + 270 && scrollpx < 0.97 * iw + 3661"
+        v-if="scrollpx > 0.29 * iw + 270 && scrollpx < 0.95 * iw + 5125"
         href="#home"
         class="hoveringButton alignLeft desktop"
         title="Voltar para o topo da página"
@@ -21,7 +21,7 @@
     </Transition>
     <Transition name="fade">
       <a
-        v-if="scrollpx < 0.97 * iw + 3661"
+        v-if="scrollpx < 0.95 * iw + 5125"
         href="https://wa.me/5511984574754?text=Olá%2C%20estive%20no%20seu%20site%20e%20tenho%20interesse%20em%20conhecer%20melhor%20vocês!"
         class="hoveringButton alignRight desktop"
         title="Contate-nos no whatsapp!"
